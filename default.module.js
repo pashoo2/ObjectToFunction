9840236d6906e29d7528b970cb9a0a3e6643dfe0
Akulich Paul <pashoo2@users.noreply.github.com>
2017-12-18 14:52:19 +0400

    Update default.module.js

--- default.module.js
+++ default.module.js
@@ -1,8 +1,6 @@
 /*eslint-disable*/
-
-
 /*
- * using a https://developer.mozilla.org/en-US/docs/DOM/document.cookie#A_little_framework.3A_a_complete_cookies_reader.2Fwriter_with_full_unicode_support as prototype for this
+ * inspired by a https://developer.mozilla.org/en-US/docs/DOM/document.cookie#A_little_framework.3A_a_complete_cookies_reader.2Fwriter_with_full_unicode_support
  * @param { object } oTarget - arget object
  * @param { string } defaultMethodName - which method to use on oTarget call
  * @returns {object.Proxy}  
@@ -12,18 +10,18 @@ export default (oTarget, defaultMethodName) => {
     ()=>{}
     , {
       "get": function (_oTarget, sKey) {
-        return oTarget[sKey] || oTarget.getItem(sKey) || undefined;
+        return oTarget[sKey] || undefined;
       },
       apply(_oTarget, thisArg, argumentsList) {
         return oTarget[defaultMethodName].apply(thisArg, argumentsList);
       },
       "set": function (_oTarget, sKey, vValue) {
         if (sKey in oTarget) { return false; }
-        return oTarget.setItem(sKey, vValue);
+        return oTarget[sKey] = vValue;
       },
       "deleteProperty": function (_oTarget, sKey) {
         if (sKey in oTarget) { return false; }
-        return oTarget.removeItem(sKey);
+        return delete oTarget[sKey];
       },
       "enumerate": function (_oTarget, sKey) {
         return oTarget.keys();
@@ -35,13 +33,15 @@ export default (oTarget, defaultMethodName) => {
         return oTarget.keys();
       },
       "has": function (_oTarget, sKey) {
-        return sKey in oTarget || oTarget.hasItem(sKey);
+        return sKey in oTarget || oTarget.hasOwnProperty(sKey);
       },
       "hasOwn": function (_oTarget, sKey) {
-        return oTarget.hasItem(sKey);
+        return oTarget.hasOwnProperty(sKey);
       },
       "defineProperty": function (_oTarget, sKey, oDesc) {
-        if (oDesc && "value" in oDesc) { oTarget.setItem(sKey, oDesc.value); }
+        if (oDesc && "value" in oDesc) {
+          Object.defineProperty(oTarget, sKey, oDesc)
+        }
         return oTarget;
       },
       "getPropertyNames": function (_oTarget) {
@@ -51,26 +51,11 @@ export default (oTarget, defaultMethodName) => {
         return Object.getOwnPropertyNames(oTarget).concat(oTarget.keys());
       },
       "getPropertyDescriptor": function (_oTarget, sKey) {
-        var vValue = oTarget[sKey] || oTarget.getItem(sKey)
-        return vValue ? {
-          "value": vValue,
-          "writable": true,
-          "enumerable": true,
-          "configurable": false
-        } : undefined;
+        return Object.getOwnPropertyDescriptor(oTarget, sKey)
       },
       "getOwnPropertyDescriptor": function (_oTarget, sKey) {
-        var vValue = oTarget.getItem(sKey);
-        return vValue ? {
-          "value": vValue,
-          "writable": true,
-          "enumerable": true,
-          "configurable": false
-        } : undefined;
-      },
-      "fix":  function (_oTarget) {
-        return "not implemented yet!";
-      },
+        return Object.getOwnPropertyDescriptor(oTarget, sKey)
+      }
     });
 };
 /*eslint-enable*/
